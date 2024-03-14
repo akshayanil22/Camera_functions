c5693ba5ef62e833370d473ca8bc5d83a05dba29
Akshaykumar2255 <akshaykumar@accruontechnologies.co.in>
2024-03-14 11:40:38 +0530

    changes in camera initialization

--- lib/take_picture.dart
+++ lib/take_picture.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+import 'package:permission_handler/permission_handler.dart';
 
 
 class TakePictureScreen extends StatefulWidget {
@@ -20,10 +21,10 @@ class TakePictureScreen extends StatefulWidget {
 class TakePictureScreenState extends State<TakePictureScreen> with WidgetsBindingObserver{
   CameraController? _controller;
   List<CameraDescription> camera = [];
-  Future<void>? _initializeControllerFuture;
   Directory? appDocumentsDir;
   List<String> images = [];
   bool isRearCameraSelected = true;
+  bool showOpenApp = false;
 
   CameraDescription? selectedCamera;
   FlashMode? selectedFlashMode = FlashMode.auto;
@@ -33,7 +34,7 @@ class TakePictureScreenState extends State<TakePictureScreen> with WidgetsBindin
   void initState(){
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    _initializeControllerFuture = initializeCamera();
+    initializeCamera();
   }
 
  Future<void> initializeCamera() async {
@@ -46,32 +47,126 @@ class TakePictureScreenState extends State<TakePictureScreen> with WidgetsBindin
       }
     }
 
-    camera = await availableCameras();
+   try{
 
-    if(camera.isNotEmpty){
-      _controller = CameraController(
-        camera.first,
-        ResolutionPreset.high,
-      );
 
-      selectedCamera = camera.first;
-      await _controller?.initialize();
+     camera = await availableCameras();
 
-      await _controller?.setFlashMode(FlashMode.auto);
+     if(camera.isNotEmpty){
 
-    }else{
+       showOpenApp = false;
+       _initializeCameraController(camera.first);
 
-      if(mounted){
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No camera Found')));
-      }
-    }
+     }else{
 
+       if(mounted){
+         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No camera Found')));
+       }
+     }
+
+
+   }catch(e){}
     setState(() {});
 
 
   }
 
 
+  Future<void> _initializeCameraController(
+      CameraDescription cameraDescription) async {
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.high,
+    );
+
+    _controller = cameraController;
+
+    // If the controller is updated then update the UI.
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+      if (cameraController.value.hasError) {
+        showInSnackBar(
+            'Camera error ${cameraController.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      switch (e.code) {
+        case 'CameraAccessDenied':
+          if(mounted){
+            await customShowDialog(title: 'Camera').then((value) async {
+              if(value!=null) {
+                await openAppSettings();
+              }
+
+            });
+          }
+          showOpenApp =true;
+          showInSnackBar('You have denied camera access.');
+        case 'CameraAccessDeniedWithoutPrompt':
+        // iOS only
+          showInSnackBar('Please go to Settings app to enable camera access.');
+        case 'CameraAccessRestricted':
+        // iOS only
+          showInSnackBar('Camera access is restricted.');
+        case 'AudioAccessDenied':
+          if(mounted){
+            await customShowDialog(title: 'Microphone').then((value) async {
+              if(value!=null) {
+                await openAppSettings();
+              }
+
+            });
+          }
+          showOpenApp =true;
+          showInSnackBar('You have denied audio access.');
+        case 'AudioAccessDeniedWithoutPrompt':
+        // iOS only
+          showInSnackBar('Please go to Settings app to enable audio access.');
+        case 'AudioAccessRestricted':
+        // iOS only
+          showInSnackBar('Audio access is restricted.');
+        default:
+          _showCameraException(e);
+          break;
+      }
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<dynamic> customShowDialog({required String title}) {
+    return showDialog(context: context, builder: (context) {
+            return AlertDialog(
+              icon: const Icon(Icons.settings),
+              title: Text("Turn on $title permission"),
+              content: Text('The $title permission is require while using the app.'),
+              actions: [
+                TextButton(onPressed: () async {
+                  Navigator.pop(context);
+                }, child: const Text("Cancel")),
+                TextButton(onPressed: () async {
+                  Navigator.pop(context,true);
+                }, child: const Text("Settings"))
+              ],
+            );
+          },);
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _showCameraException(CameraException e) {
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
 
   @override
   void dispose() {
@@ -81,29 +176,32 @@ class TakePictureScreenState extends State<TakePictureScreen> with WidgetsBindin
   }
 
   @override
-  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
-    if(state == AppLifecycleState.inactive || state == AppLifecycleState.paused){
-      _controller?.dispose();
-    }else if(state == AppLifecycleState.resumed){
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController1 = _controller;
 
-      if(camera.isNotEmpty){
-        _controller = CameraController(
-          selectedCamera??camera.first,
-          ResolutionPreset.high,
-        );
-
-        await _controller?.initialize();
-        await _controller?.setFlashMode(selectedFlashMode??FlashMode.off);
-        setState(() {});
-
-      }else{
-        if(mounted){
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No camera Found')));
-        }
-      }
+    // App state changed before we got the chance to initialize.
+    if (cameraController1 == null || !cameraController1.value.isInitialized) {
+      return;
     }
 
-    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.inactive) {
+      cameraController1.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      onNewCameraSelected(cameraController1.description);
+
+      if(camera.isNotEmpty){
+        _initializeCameraController(selectedCamera??camera.first);
+      }
+
+    }
+  }
+
+  Future<void> onNewCameraSelected(CameraDescription cameraDescription) async {
+    final CameraController? oldController = _controller;
+    if (oldController != null) {
+      _controller = null;
+      await oldController.dispose();
+    }
   }
 
   @override
@@ -111,150 +209,145 @@ class TakePictureScreenState extends State<TakePictureScreen> with WidgetsBindin
 
     return Scaffold(
       appBar: AppBar(title: const Text('Photo')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          print("sahin");
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Column(children: [
-              Expanded(child: Container(
-                clipBehavior: Clip.hardEdge,
-                margin: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12)
-                ),
-                child: Stack(
-                  children: [
-                    CameraPreview(_controller!,
-                    child: LayoutBuilder(
-                        builder: (context,constraints) {
-                          return GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTapDown: (details) {
-                              onViewFinderTap(details, constraints);
-                            },
-                          );
-                        }
-                    ),
-                    ),
-                    Positioned(
-                        right: 12,
-                        top: 12,
-                        child: Row(
-                          children: [
-                            if(camera.length>1)
-                            IconButton(onPressed: () async{
-                              isRearCameraSelected?await _controller?.setDescription(camera[1]):await _controller?.setDescription(camera[0]);
-                              selectedCamera = _controller?.description;
-                              isRearCameraSelected = !isRearCameraSelected;
-                            }, icon: SvgPicture.asset('assets/images/switch_cam.svg')),
+      body: (_controller!=null && _controller!.value.isInitialized)?Column(children: [
+          Expanded(child: Container(
+            clipBehavior: Clip.hardEdge,
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12)
+            ),
+            child: Stack(
+              children: [
 
-                            ///Flash on and off Buttons
-                            // IconButton(onPressed: ()async{
-                            //   if(_controller?.value.flashMode==FlashMode.off){
-                            //    await _controller?.setFlashMode(FlashMode.always);
-                            //    selectedFlashMode = FlashMode.always;
-                            //   }else if(_controller?.value.flashMode==FlashMode.always){
-                            //    await _controller?.setFlashMode(FlashMode.off);
-                            //    selectedFlashMode = FlashMode.off;
-                            //   }
-                            //   setState(() {});
-                            // }, icon: SvgPicture.asset('assets/images/flash_${_controller?.value.flashMode==FlashMode.off?'off':'on'}.svg')),
-                          ],
-                        ))
-                  ],
-                ),
-              )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Badge(
-                      label: Text(images.length.toString()),
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        width: 46,
-                        height: 46,
 
-                        decoration: BoxDecoration(color: const Color(0xffDDE5DA),borderRadius: BorderRadius.circular(12)),
-                        child: images.isEmpty?null:Image.file(File(images.last),fit: BoxFit.cover,),
-                      ),
-                    ),
-                  const SizedBox(width: 12,),
-                  Container(
-                    width: 56,
-                    height: 56,
-                    decoration: BoxDecoration(
-                        color: const Color(0xffB5F1BD),
-                        borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: IconButton(
-                        icon: const Icon(Icons.camera),
-                        onPressed: () async {
-                          // Take the Picture in a try / catch block. If anything goes wrong,
-                          // catch the error.
-                          try {
-                            // Ensure that the camera is initialized.
-                           // await _initializeControllerFuture;
-
-                            final image = await _controller!.takePicture();
-
-                            if(appDocumentsDir !=null){
-                              File file = File(path.join(appDocumentsDir!.path,path.basename(image.path)));
-                              await file.create();
-                              await file.writeAsBytes(await image.readAsBytes());
-                              try{
-                                File(image.path).deleteSync();
-                              }catch(e){
-                                //
-                              }
-                              setState(() {
-                                images.add(file.path);
-                              });
-                            }else{
-                              if(context.mounted){
-                                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('did not get permission to access storage')));
-                              }
-                            }
-                            if (!context.mounted) return;
-                          } catch (e) {
-                            // If an error occurs, log the error to the console.
-                          }
-                        }
-                    ),
+                CameraPreview(_controller!,
+                  child: LayoutBuilder(
+                      builder: (context,constraints) {
+                        return GestureDetector(
+                          behavior: HitTestBehavior.opaque,
+                          onTapDown: (details) {
+                            onViewFinderTap(details, constraints);
+                          },
+                        );
+                      }
                   ),
-                  const SizedBox(width: 12,),
-                  Container(
-                    width: 46,
-                    height: 46,
-                    decoration: BoxDecoration(
-                        color: const Color(0xffDDE5DA),
-                        borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: IconButton(
-                      icon: const Icon(Icons.file_upload_outlined),
-                      onPressed: (){
-                        if(images.isNotEmpty){
+                ),
+                Positioned(
+                    right: 12,
+                    top: 12,
+                    child: Row(
+                      children: [
+                        if(camera.length>1)
+                          IconButton(onPressed: () async{
+                            isRearCameraSelected?await _controller?.setDescription(camera[1]):await _controller?.setDescription(camera[0]);
+                            selectedCamera = _controller?.description;
+                            isRearCameraSelected = !isRearCameraSelected;
+                          }, icon: SvgPicture.asset('assets/images/switch_cam.svg')),
+
+                        ///Flash on and off Buttons
+                        // IconButton(onPressed: ()async{
+                        //   if(_controller?.value.flashMode==FlashMode.off){
+                        //    await _controller?.setFlashMode(FlashMode.always);
+                        //    selectedFlashMode = FlashMode.always;
+                        //   }else if(_controller?.value.flashMode==FlashMode.always){
+                        //    await _controller?.setFlashMode(FlashMode.off);
+                        //    selectedFlashMode = FlashMode.off;
+                        //   }
+                        //   setState(() {});
+                        // }, icon: SvgPicture.asset('assets/images/flash_${_controller?.value.flashMode==FlashMode.off?'off':'on'}.svg')),
+                      ],
+                    ))
+              ],
+            ),
+          )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Badge(
+                label: Text(images.length.toString()),
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  width: 46,
+                  height: 46,
+
+                  decoration: BoxDecoration(color: const Color(0xffDDE5DA),borderRadius: BorderRadius.circular(12)),
+                  child: images.isEmpty?null:Image.file(File(images.last),fit: BoxFit.cover,),
+                ),
+              ),
+              const SizedBox(width: 12,),
+              Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: const Color(0xffB5F1BD),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: IconButton(
+                    icon: const Icon(Icons.camera),
+                    onPressed: () async {
+                      // Take the Picture in a try / catch block. If anything goes wrong,
+                      // catch the error.
+                      try {
+                        // Ensure that the camera is initialized.
+                        // await _initializeControllerFuture;
+
+                        final image = await _controller!.takePicture();
+
+                        if(appDocumentsDir !=null){
+                          File file = File(path.join(appDocumentsDir!.path,path.basename(image.path)));
+                          await file.create();
+                          await file.writeAsBytes(await image.readAsBytes());
                           try{
-                            widget.onImages(images);
-                            Navigator.maybePop(context);
+                            File(image.path).deleteSync();
                           }catch(e){
                             //
                           }
+                          setState(() {
+                            images.add(file.path);
+                          });
                         }else{
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No images taken')));
+                          if(context.mounted){
+                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('did not get permission to access storage')));
+                          }
                         }
-                      },
-                    ),
-                  ),
-                ],),
-              const SizedBox(height: 16,)
-            ],);
-          } else {
-            // Otherwise, display a loading indicator.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+                        if (!context.mounted) return;
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                      }
+                    }
+                ),
+              ),
+              const SizedBox(width: 12,),
+              Container(
+                width: 46,
+                height: 46,
+                decoration: BoxDecoration(
+                    color: const Color(0xffDDE5DA),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.file_upload_outlined),
+                  onPressed: (){
+                    if(images.isNotEmpty){
+                      try{
+                        widget.onImages(images);
+                        Navigator.maybePop(context);
+                      }catch(e){
+                        //
+                      }
+                    }else{
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('No images taken')));
+                    }
+                  },
+                ),
+              ),
+            ],),
+          const SizedBox(height: 16,)
+      ],):showOpenApp?Center(
+        child: TextButton(onPressed: () async {
+          await openAppSettings();
+        }, child: const Text("Open Settings")),
+      ):const Center(child: CircularProgressIndicator()),
     );
   }
 
